07933d77d266e0f03aa3e0d3f527641c1972bfdc
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2024-01-10 21:14:08 +0545

    worked on forms and views

--- room/urls.py
+++ room/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('rooms/', views.RoomListView.as_view(), name='rooms'),
+    path('roomdetail/<int:id>/', views.RoomDetailView.as_view(), name='roomdetail'),
+    path('bookroom/', views.RoomBookView.as_view(), name='bookroom'),
 ]
