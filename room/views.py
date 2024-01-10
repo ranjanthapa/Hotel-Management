ddaaca11ff87079c4109d8f9c7189db64e3fc2d2
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2024-01-10 14:33:23 +0545

    project started

--- room/views.py
+++ room/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views import  View
+from django.views.generic import ListView, RedirectView, TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'room/home.html'
