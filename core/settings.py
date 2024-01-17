0d81574decb06b78d7d4a188590180468c144020
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2024-01-17 20:14:13 +0545

    Worked on views

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'  # Add the project-level static directory
+]
 
 
 MEDIA_URL = '/media/'
@@ -129,5 +132,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_FILES_DIRS = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR / 'static_root'
